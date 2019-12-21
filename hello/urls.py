2cb12de1d9c263788e838eeda716275eeeaa436a
UltraBirdTech <ultrabirdrepti@gmail.com>
2019-12-22 08:52:15 +0900

    add template about index

--- hello/urls.py
+++ hello/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from .views import HelloView
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path(r'', HelloView.as_view(), name='index'),
     path('next', views.next, name='next'),
     path('form', views.form, name='form'),
 ]
--- hello/views.py
+++ hello/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 from .forms import HelloForm
 
+class HelloView(TemplateView):
+    
+    def __init__(self):
+        self.params = {
+            'title': 'Hello',
+            'message': 'your data:',
+            'form': HelloForm()
+        }
+
+    def get(self, request):
+        return render(request, 'hello/index.html', self.params)
+
+    def post(self, request):
+        msg = 'name: ' + request.POST['name'] + \
+            '<br>mail: '+ request.POST['mail'] + \
+            '<br>age: '+ request.POST['age']
+        self.params['message'] = msg
+        self.params['form'] = HelloForm(request.POST)
+        return render(request, 'hello/index.html', self.params)
+        
 def index(request):
     print(request)
     params = {
