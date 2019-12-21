a4f7b2c6add020a364057eaeb115aeff105fbad8
UltraBirdTech <ultrabirdrepti@gmail.com>
2019-12-21 19:16:06 +0900

    add other page.

--- hello/views.py
+++ hello/views.py
@@ -6,5 +6,14 @@ def index(request):
     params = {
         'title': 'Hello/Index',
         'msg': 'This is a sample message',
+        'goto': 'next',
+    }
+    return render(request, 'hello/index.html', params)
+
+def next(request):
+    params = {
+        'title': 'Hello/Next',
+        'msg': 'This is a other sample page.',
+        'goto': 'next',
     }
     return render(request, 'hello/index.html', params)
