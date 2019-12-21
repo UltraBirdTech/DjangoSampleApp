c806f14418c6ba4fbd07e4f40769e687aadc21d0
UltraBirdTech <ultrabirdrepti@gmail.com>
2019-12-21 19:08:58 +0900

    add params views to templates.

--- hello/views.py
+++ hello/views.py
@@ -3,4 +3,8 @@ from django.http import HttpResponse
 
 def index(request):
     print(request)
-    return render(request, 'hello/index.html')
+    params = {
+        'title': 'Hello/Index',
+        'msg': 'This is a sample message',
+    }
+    return render(request, 'hello/index.html', params)
