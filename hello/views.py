25291d2a5869ddb445239c14f7653a28746a36af
UltraBirdTech <ultrabirdrepti@gmail.com>
2019-12-21 19:05:07 +0900

    add template file.

--- hello/views.py
+++ hello/views.py
@@ -3,9 +3,4 @@ from django.http import HttpResponse
 
 def index(request):
     print(request)
-    if 'msg' in request.GET:
-        msg = request.GET['msg']
-        result = 'you typed: "' + msg + '".'
-    else:
-        result = 'please send msg parameter!'
-    return HttpResponse(result)
+    return render(request, 'hello/index.html')
