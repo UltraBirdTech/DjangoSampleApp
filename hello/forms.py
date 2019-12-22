fcb959c9ad5076ba98f4a8b43bc24b6df462c567
UltraBirdTech <ultrabirdrepti@gmail.com>
2019-12-22 15:38:42 +0900

    add create form.

--- hello/forms.py
+++ hello/forms.py
@@ -1,5 +1,11 @@
 from django import forms
 
-class HelloForm(forms.Form):
+class HelloSearchForm(forms.Form):
     id = forms.IntegerField(label='ID')
 
+class HelloCreateForm(forms.Form):
+    name = forms.CharField(label='Name')
+    mail = forms.EmailField(label='Email')
+    gender = forms.BooleanField(label='Gender', required=False)
+    age = forms.IntegerField(label='Age')
+    birthday = forms.DateField(label='Birth')
--- hello/views.py
+++ hello/views.py
@@ -1,21 +1,39 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import redirect
 from .models import Friend
-from .forms import HelloForm
+from .forms import HelloSearchForm
+from .forms import HelloCreateForm
 
 def index(request):
-    data = Friend.objects.all()
     params = {
         'title': 'Hello',
         'message': 'all firends.',
-        'form': HelloForm,
+        'search_form': HelloSearchForm,
         'data': [],
     }
     if (request.method == 'POST'):
         num = request.POST['id']
         item = Friend.objects.get(id=num)
         params['data'] = [item]
-        params['form'] = HelloForm(request.POST)
+        params['form'] = HelloSearchForm(request.POST)
     else:
         params['data'] = Friend.objects.all()
     return render(request, 'hello/index.html', params)
+
+def create(request):
+    params = { 
+        'title': 'Hello',
+        'form': HelloCreateForm(),
+    }
+    if (request.method == 'POST'):
+        name = request.POST['name']
+        mail = request.POST['mail']
+        gender= 'gender' in request.POST
+        age = request.POST['age']
+        birth = request.POST['birthday']
+        friend = Friend(name=name, mail=mail, gender=gender, age=age, birthday=birth)
+        friend.save()
+        return redirect(to='/hello')
+    return render(request, 'hello/create.html', params)
+
