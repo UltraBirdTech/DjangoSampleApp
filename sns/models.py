c977a7e8ae10ca67bec4ee4124ab3b86f0aff7dc
UltraBirdTech <ultrabirdrepti@gmail.com>
2019-12-23 06:29:18 +0900

    add Mesasge class.

--- sns/models.py
+++ sns/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.conrib.auth.models import User
 
-# Create your models here.
+class Message(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='message_owner')
+    group = models.ForeignKey('Group', on_delete=models.CASCADE)
+    content = models.TextField(max_length=1000)
+    share_id = models.IntegerField(default=-1)
+    good_count = models.IntegerField(default=0)
+    share_count = models.IntegerField(default=0)
+    pub_date = models.DatetimeField(auto_new_add=True)
+
