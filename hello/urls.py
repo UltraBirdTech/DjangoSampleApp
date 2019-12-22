6e08b20656052670bf9173591c7503ef0dd5009c
UltraBirdTech <ultrabirdrepti@gmail.com>
2019-12-22 13:32:23 +0900

    add Friend list.

--- hello/urls.py
+++ hello/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
-from .views import HelloView
 from . import views
 
 urlpatterns = [
-    path(r'', HelloView.as_view(), name='index'),
-    path('next', views.next, name='next'),
-    path('form', views.form, name='form'),
+    path('', views.index, name='index'),
 ]
--- hello/views.py
+++ hello/views.py
@@ -1,57 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import TemplateView
-from .forms import HelloForm
+from .models import Friend
 
-class HelloView(TemplateView):
-    
-    def __init__(self):
-        self.params = {
-            'title': 'Hello',
-            'message': 'your data:',
-            'form': HelloForm()
-        }
-
-    def get(self, request):
-        return render(request, 'hello/index.html', self.params)
-
-    def post(self, request):
-        msg = 'name: ' + request.POST['name'] + \
-            '<br>mail: '+ request.POST['mail'] + \
-            '<br>age: '+ request.POST['age']
-        self.params['message'] = msg
-        self.params['form'] = HelloForm(request.POST)
-        return render(request, 'hello/index.html', self.params)
-        
 def index(request):
-    print(request)
+    data = Friend.objects.all()
     params = {
-        'title': 'Hello/Index',
-#        'msg': 'What is your name?',
-        'goto': 'next',
-        'form': HelloForm()
-    }
-    if (request.method == 'POST'):
-        params['message'] = 'name: ' + request.POST['name'] + \
-            '<br>mail: '+ request.POST['mail'] + \
-            '<br>age: '+ request.POST['age']
-        params['form'] = HelloForm(request.POST)
-
-    return render(request, 'hello/index.html', params)
-
-def next(request):
-    params = {
-        'title': 'Hello/Next',
-#        'msg': 'This is a other sample page.',
-        'goto': 'next',
-    }
-    return render(request, 'hello/index.html', params)
-
-def form(request):
-#    msg = request.POST["msg"]
-    params = {
-        'title': 'Hello/Form',
-#        'msg': 'Hello!!' + msg + '!!!',
-        'goto': 'next',
+        'title': 'Hello',
+        'message': 'all firends.',
+        'data': data,
     }
     return render(request, 'hello/index.html', params)
