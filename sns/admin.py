1743f9e13d0f525b59cea1679852fcb8ca90ab15
UltraBirdTech <ultrabirdrepti@gmail.com>
2019-12-23 20:17:03 +0900

    add models for admin.py

--- sns/admin.py
+++ sns/admin.py
@@ -1,3 +1,8 @@
+# USER:admin, Password: admin
 from django.contrib import admin
+from .models import Message, Friend, Group, Good
 
-# Register your models here.
+admin.site.register(Message)
+admin.site.register(Friend)
+admin.site.register(Group)
+admin.site.register(Good)
